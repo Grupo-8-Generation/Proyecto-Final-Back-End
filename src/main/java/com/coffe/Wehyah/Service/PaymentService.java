3a4b59fb958b7eb5b01d0a91b48b4669aa34543f
Julian <julianricardo423@gmail.com>
2024-02-26 16:46:49 -0600

    Archivos con estructura correspondiente

--- src/main/java/com/coffe/Wehyah/Service/PaymentService.java
+++ src/main/java/com/coffe/Wehyah/Service/PaymentService.java
@@ -4,8 +4,7 @@ import java.util.List;
 public interface PaymentService {
     public List<Payment> consultarPagos();
     public Payment crearPagos(Payment payment);
-    public Payment modificarPagos(Payment payment);
-    public Payment actualizarPagos(int id);
+    public Payment actualizarPagos(Payment payment);
     public Payment buscarPagos(int id);
     public void eliminarPagos(int id);
 }
